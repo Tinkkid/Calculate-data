1318e78f1f7427507b2d5677df7d094a2749e59c
Yuliia Kostovynska <y.kostovynska@gmail.com>
2024-02-20 17:11:30 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const axios = require("axios");
+
+// Посилання на файл з числами 10m.txt
+const fileURL =
+  "https://drive.google.com/file/d/1DXcHVQRLe5tqLLFgbjKS6A-j2HwRiFBM/view?usp=drive_link";
